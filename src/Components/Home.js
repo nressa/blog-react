5e09e3672ca85922ce5a238e68a9d791ebadd05a
nressa <mressanova@gmail.com>
2021-04-10 09:26:52 +0800

    Add on click event with and without params

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello World', e)
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target)
+    }
+
     return ( 
-        <div>
+        <div className="container">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Mario', e)}>Click Me Again</button>
         </div>
      );
 }
